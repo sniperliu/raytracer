b00cdbb80fe0a9529d4a21b764642bd9dae33dcd
LiuHao <sniperliuhao@gmail.com>
2020-11-16 15:05:15 +0800

    Added trilinear interpolation to smooth the noise

--- src/perlin.rs
+++ src/perlin.rs
@@ -45,13 +45,43 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f32 {
-        // FIXME f32 -> i32 may overflow
-        let i = (4. * p.x) as i32 & 255;
-        let j = (4. * p.y) as i32 & 255;
-        let k = (4. * p.z) as i32 & 255;
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
 
-        self.ran_float[(self.perm_x[i as usize] ^
-                        self.perm_y[j as usize] ^
-                        self.perm_z[k as usize]) as usize]
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+
+        let mut c: [[[f32; 2];2];2] = [[[0.; 2]; 2]; 2];
+
+        for di in 0i32..2 {
+            for dj in 0i32..2 {
+                for dk in 0i32..2 {
+                    c[di as usize][dj as usize][dk as usize] =
+                        self.ran_float[(self.perm_x[((i + di) & 255) as usize] ^
+                                        self.perm_y[((j + dj) & 255) as usize] ^
+                                        self.perm_z[((k + dk) & 255) as usize]) as usize];
+                }
+            }
+        }
+
+        trilinear_interp(c, u, v, w)
     }
+}
+
+fn trilinear_interp(c: [[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    let mut accum: f32 = 0.;
+
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                accum += (i as f32 * u + ((1 - i) as f32) * (1. - u)) *
+                    (j as f32 * v + ((1 - j) as f32) * (1. - v)) *
+                    (k as f32 * w + ((1 - k) as f32) * (1. - w)) * c[i][j][k];
+            }
+        }
+    }
+
+    accum
 }
